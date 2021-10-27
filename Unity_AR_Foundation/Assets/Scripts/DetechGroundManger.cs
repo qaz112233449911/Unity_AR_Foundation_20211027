509afb3fc9a54b49a1b2f3fccf8f568a0e71e6bf
qaz11223344991199 <qaz112233449911@gmail.com>
2021-10-27 14:22:44 +0800

    完成

--- Unity_AR_Foundation/Assets/Scripts/DetechGroundManger.cs
+++ Unity_AR_Foundation/Assets/Scripts/DetechGroundManger.cs
@@ -14,11 +14,15 @@ namespace LiangWei.ARFoundation
     public class DetechGroundManger : MonoBehaviour
     {
         [Header("點擊後要生成的物件")]
-        public GameObject goSpawn;
+        public GameObject goDragon;
         [Header("AR 射線管理器"), Tooltip("此管理器要放在攝影機 Origin 物件上")]
         public ARRaycastManager arRaycastManager;
+        [Header("生成物件要面向攝影機的物件")]
+        public Transform traCamera;
+        [Header("生成物件面相速度"), Range(0, 100)]
+        public float speedLookAt = 3.5f;
 
-        private Transform traSpawn;
+        private Transform traDragon;
         private List<ARRaycastHit> hits = new List<ARRaycastHit>();     //清單欄位 = 新增 實體 清單物件
 
         /// <summary>
@@ -39,12 +43,39 @@ namespace LiangWei.ARFoundation
         /// </summary>
         private void ClickAndDetechGround()
         {
-            if (inputMouseLeft)
+            if (inputMouseLeft)                                                                          // 如果 按下指定按鍵
             {
-                Vector2 positionMouse = Input.mousePosition;
+                Vector2 positionMouse = Input.mousePosition;                                             // 取得 點擊座標
+                
+                Ray ray = Camera.main.ScreenPointToRay(positionMouse);                                   // 將 點擊座標 轉為 射線
+                if (arRaycastManager.Raycast(ray, hits, TrackableType.PlaneWithinPolygon))               // 如果 射線 打到指定的地板物件
+                {
+                    Vector3 positionHit = hits[0].pose.position;                                         // 取得點到的座標
 
-                print("點擊座標 : " + positionMouse);
+                    if (traDragon == null)
+                    {
+                        traDragon = Instantiate(goDragon, positionHit, Quaternion.identity).transform;   // 將物件生成在點到的座標上
+                        traDragon.localScale = Vector3.one * 0.5f;
+                    }
+                    else                                                                                 // 否則 有生成過物件 就更新座標
+                    {
+                        traDragon.position = positionHit;
+                        DragonLookAtCamera();
+                    }                   
+                }
             }
+        }
+        /// <summary>
+        /// 生成物件面向攝影機
+        /// </summary>
+        private void DragonLookAtCamera()
+        {
+            Quaternion angle = Quaternion.LookRotation(traCamera.position - traDragon.position);             // 取得向量
+            traDragon.rotation = Quaternion.Lerp(traDragon.rotation, angle, Time.deltaTime * speedLookAt);   // 角度差值
+            Vector3 angleOrigial = traDragon.localEulerAngles;                                               // 取得座標
+            angleOrigial.x = 0;                                                                              // 凍結 X
+            angleOrigial.z = 0;                                                                              // 凍結 Z
+            traDragon.localEulerAngles = angleOrigial;                                                       // 更新角度
         }
     }
 }
